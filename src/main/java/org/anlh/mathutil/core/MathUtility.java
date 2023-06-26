872b535a4debdb6cfd59df03bdbf9eaa6762b1c6
anlehoang6789 <anlehoang6789@gmail.com>
2023-06-26 09:07:42 +0700

    update getf() to use recursion

--- src/main/java/org/anlh/mathutil/core/MathUtility.java
+++ src/main/java/org/anlh/mathutil/core/MathUtility.java
@@ -18,18 +18,31 @@ public class MathUtility {
     //21! vượt kiểu long, vượt quá 18 con số 0 => cài ràng buộc đầu vào cho hàm/method 
     //mình kh chấp nhận n > 20
     
+//    public static long getFactorial(int n){
+//        long product = 1; //tích ban đầu = 1
+//        
+//        if(n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0 ..20, plz.");
+//        
+//        if(n == 0 || n == 1)
+//            return 1;
+//        for (int i = 2; i <= n; i++) 
+//            product *= i;
+//        return product;
+//        
+//    }
+    //return sớm để hàm kh báo lỗi 
+    
     public static long getFactorial(int n){
-        long product = 1; //tích ban đầu = 1
+        
         
         if(n < 0 || n > 20)
             throw new IllegalArgumentException("Invalid n. n must be between 0 ..20, plz.");
         
         if(n == 0 || n == 1)
             return 1;
-        for (int i = 2; i <= n; i++) 
-            product *= i;
-        return product;
+        
+        return n * getFactorial(n - 1);
         
     }
-    //return sớm để hàm kh báo lỗi 
 }
